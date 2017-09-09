579f17bc82b76441d6153ec1494d2bda366814d1
yamatatsu <the.kinnikuman@gmail.com>
2017-09-09 16:57:02 +0900

    material-ui

--- src/containers/settings/event_form.js
+++ src/containers/settings/event_form.js
@@ -1,6 +1,9 @@
 // @flow
 import React, { Component } from 'react'
-
+import TextField from 'material-ui/TextField'
+import IconButton from 'material-ui/IconButton'
+import DoneIcon from 'material-ui-icons/Done'
+import UndoIcon from 'material-ui-icons/Undo'
 import type { EventDataType } from '../../types'
 
 type EventFormType = { eventTitle?: string }
@@ -15,13 +18,35 @@ export default class EventForm extends Component<PropsType, StateType> {
     this.state = { event: { eventTitle } }
   }
 
+  onChange(name: string, value: string) {
+    this.setState({ ...this.state, event: { [name]: value } })
+  }
+
+  onClear() {
+    const { eventTitle } = this.props.event
+    this.setState({ event: { eventTitle } })
+  }
+
   render() {
-    return <Event event={this.state.event} />
+    const onChange = (e) => {
+      const { name, value } = e.target
+      this.onChange(name, value)
+    }
+    const onClear = () => {
+      this.onClear()
+    }
+    return <Event event={this.state.event} {...{ onChange, onClear }} />
   }
 }
 
-const Event = ({ event }: { event: EventFormType }) => (
+const Event = ({ event, onChange, onClear }: { event: EventFormType, onChange: Function, onClear: Function }) => (
   <div>
-    {event.eventTitle}
+    <TextField name="eventTitle" label="イベント名" value={event.eventTitle} onChange={onChange} />
+    <IconButton aria-label="Done" color="primary">
+      <DoneIcon />
+    </IconButton>
+    <IconButton aria-label="Clear" onClick={onClear}>
+      <UndoIcon />
+    </IconButton>
   </div>
 )
---
+++ src/declares/material-ui.js
@@ -0,0 +1,21 @@
+// @flow
+declare module 'material-ui/AppBar' { declare var exports: any }
+declare module 'material-ui/Button' { declare var exports: any }
+declare module 'material-ui/Card' { declare var exports: any }
+declare module 'material-ui/Checkbox' { declare var exports: any }
+declare module 'material-ui/colors/blue' { declare var exports: any }
+declare module 'material-ui/colors/teal' { declare var exports: any }
+declare module 'material-ui/Dialog' { declare var exports: any }
+declare module 'material-ui/Form' { declare var exports: any }
+declare module 'material-ui/Grid' { declare var exports: any }
+declare module 'material-ui/IconButton' { declare var exports: any }
+declare module 'material-ui/Menu' { declare var exports: any }
+declare module 'material-ui/Radio' { declare var exports: any }
+declare module 'material-ui/styles' { declare var exports: any }
+declare module 'material-ui/styles/createMuiTheme' { declare var exports: any }
+declare module 'material-ui/styles/MuiThemeProvider' { declare var exports: any }
+declare module 'material-ui/styles/palette' { declare var exports: any }
+declare module 'material-ui/TextField' { declare var exports: any }
+declare module 'material-ui/Toolbar' { declare var exports: any }
+declare module 'material-ui/transitions/Slide' { declare var exports: any }
+declare module 'material-ui/Typography' { declare var exports: any }
