531298bd3f5f733d8a53255d20633666b7cabf04
yamatatsu <the.kinnikuman@gmail.com>
2017-09-16 09:56:46 +0900

    ヘッダー

--- src/components/templates/basic.js
+++ src/components/templates/basic.js
@@ -1,14 +1,34 @@
 // @flow
 import React from 'react'
+import Grid from 'material-ui/Grid';
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
 import { signOut } from '../../infrastructure/auth'
 
 type PropsType = { children: * }
 const BasicTemplate = ({ children }: PropsType) => (
   <div>
-    <div>
-      <button onClick={signOut}>ログアウト</button>
-    </div>
+    <Header />
     {children}
   </div>
 )
 export default BasicTemplate
+
+const Header = () => (
+  <AppBar position="static">
+    <Toolbar>
+      <Grid container justify="space-between" align="center">
+        <Grid>
+          <Typography type="title" color="inherit">
+            QWith
+          </Typography>
+        </Grid>
+        <Grid>
+          <Button color="contrast" onClick={signOut}>ログアウト</Button>
+        </Grid>
+      </Grid>
+    </Toolbar>
+  </AppBar>
+)
